6a342902bc19a2584bc4b351c00243e9f829ea48
贾翕 <jiaxi@jiaxis-MacBook-Pro.local>
2014-09-23 21:27:35 +0800

    Initial Commit

---
+++ ChatTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatTime
+//
+//  Created by 贾翕 on 14-9-23.
+//  Copyright (c) 2014年 Jesse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
